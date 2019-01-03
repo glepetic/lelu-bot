d49ee86b92fca2135d2b4c744d2e77fb7574b5c5
Gonzalo Lepetic <gonzalolepetic@hotmail.com>
2019-01-03 19:23:59 -0300

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -26,7 +26,6 @@ module.exports = {
         if(message.member.roles.some(role => role.hasPermission("ADMINISTRATOR"))){
             return true;
         }
-        message.channel.send("You are not an admin");
         return false;
     },
 
